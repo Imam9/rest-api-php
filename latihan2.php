7dc7d56e7d69fb031616bb5a26733ae4b2937efd
imam9 <imamzunaidi4@gmail.com>
2021-01-15 21:15:22 +0700

    init

---
+++ latihan2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Rest Api</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="js/script.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="img/logo.png" width = "120" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">All Menu</a>
+        <a class="nav-item nav-link" href="#">Pizza</a>
+        <a class="nav-item nav-link" href="#">Pasta</a>
+        <a class="nav-item nav-link" href="#">Nasi</a>
+        <a class="nav-item nav-link" href="#">Minuman</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+  <div class="row mt-3">
+    <div class="col">
+      <h2>All Menu</h2>
+    </div>  
+  </div>
+  <div class="row" id = "daftar-menu">
+   
+  </div>
+</div>
+
+</body>
+</html>
